a8dd33698ac0164835c5fabd87b50ca6b0509c59
Bastiaan Quast <bquast@gmail.com>
2025-03-13 16:44:43 +0100

    use video mp4

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const gamesData = {
     featured: {
         title: "Space Explorer",
         description: "Embark on an epic journey through the cosmos in this groundbreaking browser game",
-        videoUrl: "https://assets.codepen.io/t-1/flower.webm", // Replace with actual video
+        videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
         gameUrl: "#"
     },
     categories: [
@@ -13,21 +13,21 @@ const gamesData = {
                     title: "Cyber Assault",
                     description: "Fast-paced cyberpunk action game",
                     thumbnailUrl: "https://picsum.photos/300/169?random=1",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 },
                 {
                     title: "Space Warriors",
                     description: "Epic space combat simulation",
                     thumbnailUrl: "https://picsum.photos/300/169?random=2",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 },
                 {
                     title: "Ninja Rush",
                     description: "Stealth action platformer",
                     thumbnailUrl: "https://picsum.photos/300/169?random=3",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 }
             ]
@@ -39,21 +39,21 @@ const gamesData = {
                     title: "Mind Bender",
                     description: "Challenge your brain with complex puzzles",
                     thumbnailUrl: "https://picsum.photos/300/169?random=4",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 },
                 {
                     title: "Color Match",
                     description: "Fast-paced color matching puzzle",
                     thumbnailUrl: "https://picsum.photos/300/169?random=5",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 },
                 {
                     title: "Logic Master",
                     description: "Test your logical thinking",
                     thumbnailUrl: "https://picsum.photos/300/169?random=6",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 }
             ]
@@ -65,21 +65,21 @@ const gamesData = {
                     title: "Lost Temple",
                     description: "Explore ancient ruins and solve mysteries",
                     thumbnailUrl: "https://picsum.photos/300/169?random=7",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 },
                 {
                     title: "Ocean Explorer",
                     description: "Underwater adventure game",
                     thumbnailUrl: "https://picsum.photos/300/169?random=8",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 },
                 {
                     title: "Sky Kingdom",
                     description: "Fantasy adventure in the clouds",
                     thumbnailUrl: "https://picsum.photos/300/169?random=9",
-                    videoUrl: "https://assets.codepen.io/t-1/flower.webm",
+                    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
                     gameUrl: "#"
                 }
             ]
